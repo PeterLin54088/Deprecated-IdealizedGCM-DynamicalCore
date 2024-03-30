875856fe6748ca39fbbd45bf33bdcfa50f3a22aa
HSUAN-CHENG LIN <r12229008@ntu.edu.tw>
2024-03-30 21:10:51 +0800

    update project dependencies

--- src/dynamical_core.jl
+++ src/dynamical_core.jl
@@ -1,19 +1,13 @@
-__precompile__(true)
 module dynamical_core
-
-using Revise
-using LinearAlgebra
-using FFTW
-using Statistics 
-using JLD2
-using MAT
-using Random
-import PyPlot
-
 """
 TODO : Seperate SW and generalize method
 """
 
+using FFTW
+using JLD2
+
+################################################################################
+# Shared
 include("shared/Atmo_Data.jl")
 include("shared/Dyn_Data.jl")
 include("shared/Gauss_And_Legendre.jl")
@@ -22,5 +16,15 @@ include("shared/Vert_Coordinate.jl")
 include("shared/Time_Integrator.jl")
 include("shared/Semi_Implicit.jl")
 include("shared/Press_And_Geopot.jl")
+
+################################################################################
+# Shallow Water System
+include("shallow_water/Output_Manager.jl")
+include("shallow_water/Shallow_Water_Physics.jl")
+include("shallow_water/Shallow_Water_Dynamics.jl")
+include("shallow_water/Initial_Condition_Controller.jl")
+
+################################################################################
+# ?
 
 end
